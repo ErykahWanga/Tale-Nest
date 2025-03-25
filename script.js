98bf14de16aced2d7f751914241865e5bf425a33
Erykah <erkahwanga@gmail.com>
2025-03-25 09:31:04 +0300

    initialize script.js and set up event listener for DOMContentLoaded

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const mainContent = document.querySelector("main");
+    const booksContainer = document.getElementById("books-container");
+    const addBookBtn = document.getElementById("addBookBtn");
+
+   
